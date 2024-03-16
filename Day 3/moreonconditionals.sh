cb2d209a40c42227ce202121d12d9e4764a29b18
LS <lisshu94@live.com>
2024-03-16 17:12:38 +0530

    Problem 8: More on Conditionals

---
+++ Day 3/moreonconditionals.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+read X
+read Y
+read Z
+if [[ $X == $Y && $Y == $Z ]]; then
+    echo "EQUILATERAL" 
+elif [[ $X == $Y || $Y == $Z || $X == $Z ]]; then
+    echo "ISOSCELES"  
+else
+    echo "SCALENE"
+fi
